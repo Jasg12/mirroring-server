703a692ff08bf80b88be9ae3c434a14e80ad29a0
Aliaksei Matsarski <alex.matsarski@getzephyr.com>
2018-11-05 21:57:08 -0800

    Init project

---
+++ src/main/java/com/sjsu/cmpe/sstreet/mirroringserver/MirroringServerApplication.java
@@ -0,0 +1,12 @@
+package com.sjsu.cmpe.sstreet.mirroringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirroringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirroringServerApplication.class, args);
+	}
+}
---
+++ src/main/java/com/sjsu/cmpe/sstreet/mirroringserver/controller/AdminController.java
@@ -0,0 +1,7 @@
+package com.sjsu.cmpe.sstreet.mirroringserver.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+}
