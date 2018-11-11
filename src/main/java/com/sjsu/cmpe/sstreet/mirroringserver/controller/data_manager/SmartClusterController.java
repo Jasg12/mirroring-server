f835f3b76a9a8129e5ad398cf58e192354f824af
Jason G <jjgonsalves7@gmail.com>
2018-11-11 12:59:05 -0800

    Data manager controller and services

---
+++ src/main/java/com/sjsu/cmpe/sstreet/mirroringserver/controller/data_manager/SmartClusterController.java
@@ -0,0 +1,11 @@
+package com.sjsu.cmpe.sstreet.mirroringserver.controller.data_manager;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SmartClusterController {
+
+
+}
+
+
